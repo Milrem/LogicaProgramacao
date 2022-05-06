60f95f59327df5fd0804c5753f97f400a3841ba3
Cristiano Neves <cris.a.neves@gmail.com>
2022-05-05 23:20:57 -0300

    First class source code

---
+++ src/Aula01.java
@@ -0,0 +1,15 @@
+public class Aula01 {
+    public static void main(String[] args) {
+//        System.out.print("Hello ");
+//        System.out.println("World");
+//        System.out.print("3 ");
+        System.out.println("Hello World");
+        System.out.print("10 / 5 = ");
+        System.out.println(10 / 5);
+        System.out.print("10 / 2 = ");
+        System.out.println(10 / 2);
+        System.out.println("Hello World");
+        System.out.println(5.0 / 2);
+//        System.out.print("4 ");
+    }
+}
