e9d953e1e55eccad293ec61f286bb58ca8bc0026
Cristiano Neves <cris.a.neves@gmail.com>
2022-05-12 22:49:27 -0300

    Desafio Da Forca

---
+++ src/DesafioDaForca.java
@@ -0,0 +1,29 @@
+/**
+ * O jogo da forca é um jogo popular que pode ser jogado simplesmente com um pedaço de papel e lápis.
+ * As origens deste jogo não são conhecidas com rigor, mas alguns autores defendem que o jogo surgiu na época Vitoriana,
+ * no Reino Unido, sendo possível encontrar referências a este jogo em diversas obras, embora com nomes diferentes.
+ *
+ * No jogo da forca um jogador tem que descobrir uma palavra escondida, tendo como única informação prévia o número de
+ * letras que constituem a palavra. O jogador procede então, por tentativa e erro, sugerindo letras que podem pertencer
+ * à palavra.
+ *
+ * Quando uma letra pertence à palavra, é indicada ao jogador as posições em que a letra aparece na palavra. Quando a
+ * letra não pertence à palavra, é incrementado um contador de tentativas (normalmente representado através do desenho
+ * de um indivíduo sendo enforcado).
+ *
+ * O jogo termina quando o jogador descobre todas as letras da palavra escondida (vencendo o jogo) ou quando esgota o
+ * número de tentativas falhas (7 tentativas, sendo enforcado ou perdendo o jogo).
+ *
+ * Crie um programa em Java que implemente a lógica necessária para permitir ao usuário jogar o jogo da forca no
+ * computador. O programa selecionará aleatoriamente uma palavra de seu dicionário para que o usuário a descubra.
+ * Observe os requisitos a seguir:
+ * - O dicionário deve conter no mímimo 10 palavras e cada uma deve conter o mínimo de 5 letras;
+ * - O jogo deve iniciar mostrando um menu para o usuário escolher iniciar uma nova partida ou fechar o jogo;
+ * - Ao iniciar uma nova partida o jogo deve selecionar uma das palavras no dicionário e exibir ao usuário o número de
+ * letras totais na palavra;
+ * - Quando a partida terminar o jogo deve mostrar uma mensagem informando se o usuário venceu ou perdeu e aguardar o
+ * usuário prescionar uma tecla para exibir novamente o menu inicial.
+ *
+ */
+public class DesafioDaForca {
+}
